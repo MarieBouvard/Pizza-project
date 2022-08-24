f96ae85517e87eb6896fe5cc6ca4f1b6477c3921
Marie Bouvard <marie.bouvard@capgemini.com>
2022-08-24 15:56:22 +0200

    Add of pizza list and CSS styles

---
+++ menu/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+
+# Create your views here.
+
+def index(request):
+    # pizzas = Pizza.objects.all()
+    # pizzas = [pizza.nom + " : " + str(pizza.prix) + " €" for pizza in pizzas]
+    # pizzas_str = ", ".join(pizzas)
+    # return HttpResponse("Les pizzas " + pizzas_str + ".")
+    pizzas = Pizza.objects.all()
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
